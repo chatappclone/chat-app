0c61a4425af95aaf7f91b355833c6fec11f49b75
David Gridley <davidgridley21@gmail.com>
2018-10-25 10:00:19 +0100

    updated database sql

--- database.sql
+++ database.sql
@@ -3,9 +3,10 @@ CREATE DATABASE chat_app;
 CREATE TABLE users (
 id SERIAL PRIMARY KEY,
 username varchar(100) NOT NULL UNIQUE,
-password text NOT NULL UNIQUE
+password text NOT NULL 
 );
 
 INSERT INTO users (username, password) VALUES ('john', 'johnpassword');
 INSERT INTO users (username, password) VALUES ('jane', 'janepassword');
 INSERT INTO users (username, password) VALUES ('testuser', '$2b$10$K9wczwMIX5eYG0vi5kFt..44TYdZBSbFzecAvXbmg1BiHGC9Lccfy');
+INSERT INTO users (username, password) VALUES ('testuser2', '$2b$10$uRMdWmjAzvsqOZoMiddxse8zsu1.M426lFLuEEvRDENeVNgnlStsi');
