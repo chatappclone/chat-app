dcc9b3fe7659348298f7f1df2a0a27891b15d952
mattc0llier <matt.collier@hotmail.com>
2018-10-26 12:13:51 +0100

    basic version copied over

--- src/components/NewChats.js
+++ src/components/NewChats.js
@@ -12,6 +12,11 @@ class NewChats extends React.Component {
   render(){
     return(
       <div>
+        <nav className="user-previews__nav">
+          <div className="user-previews__edit">Edit</div>
+          <div><h4>New Chat</h4></div>
+          <div className="user-previews__new">Cancel</div>
+        </nav>
         {this.props.otherUsers.map(user => <NewChat key={user.id} user={user} getNewChatUser={this.props.getNewChatUser}/>)}
       </div>
     )
