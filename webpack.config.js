20dcaa472160de46d9c635fc78535fdd5690a474
ytknrgn <42815334+ytknrgn@users.noreply.github.com>
2018-12-01 08:55:00 +0000

    webpack dotenv working

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
 const webpack = require('webpack');
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv');
 
 module.exports = () => {
-  const envKeys = Object.keys(process.env).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(process.env[next]);
+  const env = dotenv.config().parsed;
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
     return prev;
   }, {});
 
@@ -13,31 +14,31 @@ module.exports = () => {
     devtool: 'source-map',
     output: {
       filename: 'bundle.js',
-      path: path.resolve(__dirname, 'static')
+      path: path.resolve(__dirname, 'static'),
     },
-    plugins: [
-      new webpack.DefinePlugin(envKeys)
-    ],
+    plugins: [new webpack.DefinePlugin(envKeys)],
     module: {
       rules: [
         {
           test: /\.js$/,
           exclude: /(node_modules)/,
-          loader: require.resolve('babel-loader')
+          loader: require.resolve('babel-loader'),
         },
         {
           test: /\.scss$/,
           use: [
             {
-              loader: "style-loader"
-            }, {
-              loader: "css-loader"
-            }, {
-              loader: "sass-loader"
-            }
-          ]
-        }
-      ]
-    }
+              loader: 'style-loader',
+            },
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'sass-loader',
+            },
+          ],
+        },
+      ],
+    },
   };
-}
+};
